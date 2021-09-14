639b50ed61fec0cf91e01ea138b89a1797672624
Jeffrey Flynn <57778968+TheDarkIight@users.noreply.github.com>
2021-09-14 16:36:18 -0400

    Merging Skeleton Design
    Importing Both Design Elements Into Original Branch

--- javaMain/src/DiagramModel.java
+++ javaMain/src/DiagramModel.java
@@ -15,11 +15,12 @@ import java.util.HashMap;
  *  boolean ans = ______.contains(_____);
  */
 public class DiagramModel {
-   private ArrayList<UMLClass> diagram = new ArrayList<UMLClass>();
-    
+
+   private ArrayList<UMLClass> diagramOLD = new ArrayList<UMLClass>();
+private HashMap<String, UMLClass> diagram = new HashMap<String, UMLClass>();
     public void addClass(String name){
-         UMLClass holder = new UMLClass(name);
-         diagram.add(holder);
+        UMLClass holder = new UMLClass(name);
+        diagram.put(name, holder);
     }
 
     /**
@@ -51,8 +52,31 @@ public class DiagramModel {
 
     }
 
-    public void ListClass(String entry){
+    public void listClass(String className)
+    {
+        if (checkExistence(className, null, null, null) == "true"){
+            UMLClass input = diagram.get(className);
 
+            System.out.println("Name: \n" + input.getName());
+            System.out.println("Attributes: ");
+
+            /** Prints attributes, prints special message if none */
+            if (input.getAttributes().size() == 0)
+            {
+                System.out.println("There are no attributes in this class.");
+            }
+            else
+            {
+                for (int i = 0; i < input.getAttributes().size(); i++)
+                {
+                    System.out.println(input.getAttributes().get(i));
+                }
+            }
+        }
+        else
+        {
+            checkExistenceCLIOut(className, null, null, null);
+        }
     }
 
     public void ListClasses(){
@@ -87,4 +111,102 @@ public class DiagramModel {
         }
         return null;
     }
+
+    public void renameUMLClass(String oldName, String newName)
+    {
+        String oldNameExists = checkExistence(oldName, null, null, null);
+        String newNameExists = checkExistence(newName, null, null, null);
+        if (oldNameExists == "true" && newNameExists != "true")
+        {
+            UMLClass renamedClass = diagram.get(oldName);
+            renamedClass.renameClass(newName);
+            diagram.remove(oldName);
+            diagram.put(newName, renamedClass);
+        }
+        else if (oldNameExists != "true")
+        {
+            System.out.println("The class to be renamed, " + oldName + ", does not exist.");
+        }
+        else 
+        {
+            System.out.println("The new name \"" + newName + "\" for the class " + 
+                oldName + " already exists as a class.");
+        }
+    }
+
+    public String checkExistence(String classSrcInput, String classDestInput, String traitName, String traitType){
+        UMLClass classSrc = diagram.get(classSrcInput);
+        UMLClass classDest = diagram.get(classDestInput);
+        String doesExist = "true";
+
+        if (classSrc == null)
+        {
+            /** Updates doesExist if the source class DNE */
+            doesExist = "classSrcDNE";
+        }
+        else if (traitType != null)
+        {
+            switch (traitType) 
+            {
+                case "attribute":
+                    /** Finds index of desired attribute in source class */
+                    int targetAttributeIndex = classSrc.getAttributes().indexOf(traitName);
+                    if (targetAttributeIndex == -1)
+                    {
+                        doesExist = "attributeDNE";
+                    }
+                    break;
+                
+                case "relationship":
+                    if (classDest == null)
+                    {
+                        doesExist = "classDestDNE";
+                    }
+                    else
+                    {
+                        // Need to search relationship list for this relationship
+                    }
+                    break;
+                default:
+                    break;
+                    
+            }
+        }
+        return doesExist;        
+    }
+
+    /**
+     * Partners with checkExistence to print terminal outputs for when the class/trait DNE.
+     * This is in a separate method from checkExistence so that a future isValidName function 
+     * can check if a name is a duplicate by using the checkExistence function.  
+     * 
+     * @param classSrcInput
+     * @param classDestInput
+     * @param traitName
+     * @param traitType
+     */
+    public void checkExistenceCLIOut(String classSrcInput, String classDestInput, String traitName, String traitType)
+    {
+        String doesExist = checkExistence(classSrcInput, classDestInput, traitName, traitType);
+
+        if (doesExist != "true")
+        {
+            switch (doesExist)
+            {
+                case "classSrcDNE":
+                    System.out.println("Error: The source class " + classSrcInput + " does not exist.");
+                    break;
+                case "classDestDNE":
+                    System.out.println("Error: The destination class " + classDestInput + " does not exist.");
+                    break;
+                case "attributeDNE":
+                    System.out.println("Error: The attribute " + traitName + " does not exist in the class " 
+                    + classSrcInput + ".");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
+
