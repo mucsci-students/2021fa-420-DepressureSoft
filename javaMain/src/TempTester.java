7b75655da49dbf129affc729e617bfee2fe67fcc
jeffc1020 <jeffc2000@outlook.com>
2021-09-29 15:03:52 -0400

    Added the ability to add/delete relationships and change their types to the command line interface

--- javaMain/src/TempTester.java
+++
@@ -1,9 +0,0 @@
-public class TempTester {
-    public static void main(String[] args) {
-        DiagramModel diagram = new DiagramModel();
-        diagram.addClass("Class1");
-        diagram.addClass("Class2");
-        diagram.addRelationship("Class1", "Class2", Relationship.RelationshipType.AGGREGATION);
-        diagram.ListRelationships();
-    }
-}
