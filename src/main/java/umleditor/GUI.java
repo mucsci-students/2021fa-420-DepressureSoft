7ab0bef6e19d4e0c176f9b9dc4f9959a6a91437d
Jeffrey Flynn <57778968+TheDarkIight@users.noreply.github.com>
2021-10-01 16:57:53 -0400

    Fix package decl for GUI and format enhancement

--- src/main/java/umleditor/GUI.java
+++ src/main/java/umleditor/GUI.java
@@ -1,3 +1,5 @@
+package umleditor;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/main/java/umleditor/startupGUI.java
+++ src/main/java/umleditor/startupGUI.java
@@ -1,4 +1,5 @@
 package umleditor;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
