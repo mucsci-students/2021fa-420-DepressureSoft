8e5bc15649541a3e874eddfdb959dcea60647a2f
jeffc1020 <33158249+jeffc1020@users.noreply.github.com>
2021-10-08 16:13:51 -0400

    Lots of fixes for develop. Removed maven artifacts (#41)
    * Update .gitignore
    
    * testing
    
    * Fix gitignore and remove junk
    
    * avoids opening the startupGUI again after exiting from the cli interface
    
    Co-authored-by: djach7 <dave.jach7@gmail.com>
    Co-authored-by: Will Killian <william.killian@outlook.com>

--- src/main/java/umleditor/startupGUI.java
+++ src/main/java/umleditor/startupGUI.java
@@ -9,51 +9,46 @@ public class startupGUI {
      * Starts the program launcher GUI which gives the user two options between Console and GUI.
      */
     public static void main(String args[]){
-        JFrame frame = new JFrame("UML Program Launcher");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,110);
-        frame.setLocationRelativeTo(null);
-      
-        //Set up the necessary content pane.
-        Container pane = frame.getContentPane();
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-
-        //Creates text above buttons and edits the appearance. 
-        JLabel question = new JLabel("Which method would you like to use?");
-        question.setAlignmentX(pane.CENTER_ALIGNMENT);
-        question.setPreferredSize(new Dimension(20,20));
-        pane.add(question);
-
-        //Creates buttons and edits their appearance. 
-        JButton button = new JButton("GUI");
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        pane.add(button);
-        button.setPreferredSize(new Dimension(30,30));
-        button.add(Box.createHorizontalGlue());
-
-        JButton button2 = new JButton("Console");
-        button2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        pane.add(button2);
-        button2.setPreferredSize(new Dimension(30,30));
-        button2.add(Box.createHorizontalGlue());
-        
-        //Adds Action Listener to Buttons so when clicked they preform necessary tasks. 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-               //NOTHING YET
-            }
-         });
-
-        button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                UMLInterface.main(args);
-            }
-         });
-
-        //Displays the window.
-        frame.setVisible(true);
-     }
+    	if (args.length > 0) {
+    		if (args[0].equals("--cli")) {
+        		UMLInterface.main(args);
+        		System.exit(0);
+    		}
+    	} else {
+    	
+	        JFrame frame = new JFrame("UML Program Launcher");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setLocationRelativeTo(null);
+	      
+	        //Set up the necessary content pane.
+	        JPanel pane = new JPanel();
+	        frame.setContentPane(pane);
+	
+	        //Creates text above buttons and edits the appearance. 
+	        JLabel question = new JLabel("Which method would you like to use?");
+	        question.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(question);
+	
+	        //Creates buttons and edits their appearance. 
+	        JButton button = new JButton("GUI");
+	        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(button);
+	
+	        JButton button2 = new JButton("Console");
+	        button2.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(button2);
+	        
+	        //Adds Action Listener to Buttons so when clicked they preform necessary tasks. 
+	        button.addActionListener(e -> {
+	        	frame.dispose();
+	        });
+	        button2.addActionListener(e -> {
+	            frame.dispose();
+	            UMLInterface.main(args);
+	        });
+	        frame.pack();
+	        frame.setVisible(true);
+    	}
+    }
 }
   
